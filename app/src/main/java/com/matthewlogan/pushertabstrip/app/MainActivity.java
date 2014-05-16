acf9d24826abdd75c80336b4a20f47bbbdeca8d7
mattlogan <matt.b.logan@gmail.com>
2014-05-16 10:38:06 -0700

    initial commit

---
+++ app/src/main/java/com/matthewlogan/pushertabstrip/app/MainActivity.java
@@ -0,0 +1,68 @@
+package com.matthewlogan.pushertabstrip.app;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ColorAdapter());
+    }
+
+    private class ColorAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem (ViewGroup container, int position) {
+            View view = new View(mContext);
+
+            int backgroundColor = 0;
+            switch (position) {
+                case 0:
+                    backgroundColor = Color.BLUE;
+                    break;
+                case 1:
+                    backgroundColor = Color.RED;
+                    break;
+                case 2:
+                    backgroundColor = Color.GREEN;
+                    break;
+            }
+            view.setBackgroundColor(backgroundColor);
+
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public void destroyItem (ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
---
+++ app/src/main/java/com/matthewlogan/pushertabstrip/app/PusherTabStrip.java
@@ -0,0 +1,21 @@
+package com.matthewlogan.pushertabstrip.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+
+public class PusherTabStrip extends HorizontalScrollView {
+
+    public PusherTabStrip(Context context) {
+        super(context);
+    }
+
+    public PusherTabStrip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PusherTabStrip(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
